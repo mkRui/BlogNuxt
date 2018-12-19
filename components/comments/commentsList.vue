af885b01b9ed93b84e023e471c2c9acd3c3e2f79
shicongrui <1102163949@qq.com>
2018-12-19 08:58:59 +0800

    修改部分布局

--- components/comments/commentsList.vue
+++ components/comments/commentsList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="comments-list">
     <transition-group tag="ul" mode="out-in" name="list">
-      <li v-for="item in commentsList" :key="item.id" class="comments-main">
+      <li v-for="(item, index) in commentsList" :key="item.id" class="comments-main">
           <div class="user-face">
             <!-- userface -->
             <img :src="item.face ? item.face : userFace" alt="">
@@ -19,7 +19,29 @@
             <!-- 点赞 回复 展开 -->
             <div class="fun-button">
               <i class="iconfont icon-like" @click="like(item.id)" :class="commentsId.includes(item.id.toString()) ? 'isLike' : ''">{{ item.commentsPraise }}</i>
+              <i class="iconfont icon-comments">{{ Number(item.commentsNum) }}</i>
+              <i class="iconfont icon-reply iconReply" @click="reply(index)">回复</i>
+              <i class="el-icon-close" v-if="locationIndex === index" @click="locationIndex = ''">取消</i>
             </div>
+
+            <!-- 回复主评论的 回复框 -->
+            <comments v-if="locationIndex === index" ></comments>
+
+            <!-- 已经评论过的 -->
+            <transition-group tag="ol" mode="out-in" name="list">
+              <li v-for="elem in commentsList" :key="elem.id" class="comments-child">
+                <p>{{ item.commentsUser }} @ {{ item.commentsUserMin ? item.commentsUserMin : '匿名' }}:</p>
+                <!-- 评论内容支持 markedown 语法 -->
+                <view-marked :marked="item.commentsContent | emoji"></view-marked>
+                
+                <!-- 子集评论列表 -->
+                <div class="fun-button">
+                  <i class="iconfont icon-like" @click="like(item.id)" :class="commentsId.includes(item.id.toString()) ? 'isLike' : ''">{{ item.commentsPraise }}</i>
+                  <i class="iconfont icon-reply iconReply" @click="reply(index)">回复</i>
+                  <i class="el-icon-close" v-if="locationIndex === index" @click="locationIndex = ''">取消</i>
+                </div>
+              </li>
+            </transition-group>
           </div>
       </li>
     </transition-group>
@@ -30,16 +52,20 @@ import userFace from '@/assets/image/people.jpeg'
 
 import viewMarked from '@/components/common/viewMarked.vue'
 
+import comments from './comments.vue'
+
 export default {
   name: 'comments-list',
   data () {
     return {
       userFace: userFace,
-      commentsId: []
+      commentsId: [],
+      locationIndex: ''
     }
   },
   components: {
-    viewMarked
+    viewMarked,
+    comments
   },
   computed: {
     commentsList () {
@@ -47,6 +73,7 @@ export default {
     }
   },
   methods: {
+    // 评论点赞
     like (item) {
       let like = (window.localStorage.getItem('commentsLike') || '').split(',')
       if (!like.includes(item.toString())) {
@@ -55,6 +82,10 @@ export default {
       }
       console.log(this.commentsId)
       window.localStorage.setItem('commentsLike', like.join(','))
+    },
+    // 回复评论
+    reply (item) {
+      this.locationIndex = item
     }
   },
   mounted () {
@@ -69,6 +100,42 @@ export default {
     margin-top: 30px;
     display: flex;
     justify-content: space-between;
+    ol {
+      margin-top: 5px;
+      background: #e7eaf1;
+      padding: 10px;
+      li {
+        line-height: 2;
+        border-bottom: 1px dotted #fff;
+        >.fun-button {
+          display: flex;
+          justify-content: flex-start;
+          align-items: center;
+          i {
+            margin-right: 10px;
+            color: #666;
+            font-size: 14px;
+            &::before {
+              margin-right: 3px;
+              font-size: 16px;
+            }
+            &.isLike {
+              color: #F56C6C;
+            }
+            &.iconReply {
+              display: none;
+            }
+          }
+        }
+        &:hover {
+          > .fun-button {
+            .iconReply {
+              display: block;
+            }
+          }
+        }
+      }
+    }
     > div {
       &.user-face {
         width: 60px;
@@ -83,22 +150,37 @@ export default {
       }
       &:nth-child(2) {
         width: 88%;
-        p {
+        > p {
           display: flex;
           justify-content: space-between;
           color: #9e9e9e;
           margin-bottom: 10px;
         }
-        .fun-button {
+        > .fun-button {
           margin-top: 10px;
+          display: flex;
+          justify-content: flex-start;
+          align-items: center;
           i {
+            margin-right: 10px;
             color: #666;
-            font-size: 16px;
+            font-size: 14px;
             &::before {
               margin-right: 3px;
+              font-size: 16px;
             }
             &.isLike {
               color: #F56C6C;
+            }
+            &.iconReply {
+              display: none;
+            }
+          }
+        }
+        &:hover {
+          > .fun-button {
+            .iconReply {
+              display: block;
             }
           }
         }
